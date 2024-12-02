c3cbc7e77d44649f8823499a249a03cd6db1d10e
Andres sierra garcia <andresfelipe3666@gmail.com>
2024-12-02 11:35:50 -0500

    fdscs

--- index.js
+++ index.js
@@ -2,37 +2,40 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-import httpTerceros from './routes/terceros.js'
-import httpMovimiento from './routes/movimiento.js'
-import httpArticulos from './routes/articulos.js'
-import httpCategorias from './routes/categorias.js'
-import httpUsuarios from './routes/usuarios.js'
+import httpTerceros from './routes/terceros.js';
+import httpMovimiento from './routes/movimiento.js';
+import httpArticulos from './routes/articulos.js';
+import httpCategorias from './routes/categorias.js';
+import httpUsuarios from './routes/usuarios.js';
 import cors from 'cors';
 
-
-
-
 const app = express();
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'x-token'],
-        credentials: true
-    }
-));
+
+// Configuración de CORS
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'x-token'],
+    credentials: true
+}));
+
+// Middleware para parsear JSON
 app.use(express.json());
+
+// Rutas de la API
 app.use("/api/terceros", httpTerceros);
 app.use("/api/movimientos", httpMovimiento);
 app.use("/api/articulos", httpArticulos);
 app.use("/api/categorias", httpCategorias);
 app.use("/api/", httpUsuarios);
+
+// Servir archivos estáticos desde la carpeta 'public' (donde copiaste los archivos de dist)
 app.use(express.static('public'));
 
-
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+    
     mongoose.connect(process.env.CNX_MONGO)
-    .then(()=> console.log('conected!'))
-    .catch((error)=> console.log(error)) 
-})
+        .then(() => console.log('Conectado a la base de datos!'))
+        .catch((error) => console.log(error));
+});
